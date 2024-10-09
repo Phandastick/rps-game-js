a286aa01986b98c222ee2296924cdcbdeb86d747
Phandastick <127112633+Phandastick@users.noreply.github.com>
2024-10-09 10:00:13 +0800

    corrected rps logic again, im stupid

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const hiddenValue = document.getElementById('hdChoice');
+const txtDisplayResult = document.getElementById('display-result')
 
 rps = {
     1:'rock',
@@ -9,9 +10,9 @@ rps = {
 async function getRandomChoice(){
     let random = Math.random() * 3;
     let num = Math.ceil(random)
-    let result = rps[num]
+    AIChoice = rps[num]
 
-    return await result;
+    return await AIChoice;
 }
 
 function compareRPS(playerChoice, randomChoice){
@@ -24,14 +25,17 @@ function compareRPS(playerChoice, randomChoice){
             if(randomChoice === 'paper'){
                 return 'win'
             };
+            break
         case 'paper':
             if(randomChoice === 'rock'){
                 return 'win'
             };
+            break
         case 'rock':
             if(randomChoice === 'scissors'){
                 return 'win'
-            }
+            };
+            break
         default:
             return 'loss'
     }
@@ -49,21 +53,42 @@ async function challengerps(numChoice){
     if(result === 'win'){
         //win
         console.log('You win!')
-        return
+        return 'win'
     } else if(result === 'draw'){
         //draw
         console.log('Its a draw :o')
-        return
+        return 'draw'
     } else {
         //loss
         console.log('You lost :(')
-        return
+        return 'loss'
     }
 }
 
 function challenge(){
     playerChoice = hiddenValue.value
-    challengerps(parseInt(playerChoice));
+    challengerps(parseInt(playerChoice)).then((result) => {
+        switch (result){
+            case 'win':
+                txtDisplayResult.textContent = 'AI Choice: ' + AIChoice + '. You won :D';
+                break
+
+            case 'draw':
+                txtDisplayResult.textContent = 'AI Choice: ' + AIChoice + '. You drew :o';
+                break
+
+            case 'loss':
+                txtDisplayResult.textContent = 'AI Choice: ' + AIChoice + '. You loss D:';
+                break
+            default:
+                txtDisplayResult.textContent = 'Unexpected error!';
+                break
+        }
+    });
+}
+
+function updateChoice(){
+    document.getElementById('display-choice').innerHTML = 'Your choice: ' + rps[hiddenValue.value];
 }
 
 function initListeners(){
@@ -74,16 +99,19 @@ function initListeners(){
     rockChoice.addEventListener("click", function(){
         console.log('> You chose rock!')
         hiddenValue.value = 1;
+        updateChoice()
     })
 
     paperChoice.addEventListener("click", function(){
         console.log('> You chose paper!')
         hiddenValue.value = 3;
+        updateChoice()
     })
 
     scissorsChoice.addEventListener("click", function(){
         console.log('> You chose scissors!')
         hiddenValue.value = 2;
+        updateChoice()
     })
 }
 
